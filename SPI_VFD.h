1ec52d3981da9133b6a22921dd27064fc21ce9e0
J.P. McGlinn <jmcglinn@speakeasy.net>
2011-08-18 00:18:24 -0700

    Cleaned up library, added brightness, fixed little bugs. See README.txt.

--- SPI_VFD.h
+++ SPI_VFD.h
@@ -1,61 +1,62 @@
-#ifndef LiquidCrystal_h
-#define LiquidCrystal_h
+#ifndef SPI_VFD_h
+#define SPI_VFD_h
 
 #include <inttypes.h>
 #include "Print.h"
 
 // commands
-#define LCD_CLEARDISPLAY 0x01
-#define LCD_RETURNHOME 0x02
-#define LCD_ENTRYMODESET 0x04
-#define LCD_DISPLAYCONTROL 0x08
-#define LCD_CURSORSHIFT 0x10
-#define LCD_FUNCTIONSET 0x20
-#define LCD_SETCGRAMADDR 0x40
-#define LCD_SETDDRAMADDR 0x80
+#define VFD_CLEARDISPLAY 0x01
+#define VFD_RETURNHOME 0x02
+#define VFD_ENTRYMODESET 0x04
+#define VFD_DISPLAYCONTROL 0x08
+#define VFD_CURSORSHIFT 0x10
+#define VFD_FUNCTIONSET 0x30
+#define VFD_SETCGRAMADDR 0x40
+#define VFD_SETDDRAMADDR 0x80
 
 // flags for display entry mode
-#define LCD_ENTRYRIGHT 0x00
-#define LCD_ENTRYLEFT 0x02
-#define LCD_ENTRYSHIFTINCREMENT 0x01
-#define LCD_ENTRYSHIFTDECREMENT 0x00
+#define VFD_ENTRYRIGHT 0x00
+#define VFD_ENTRYLEFT 0x02
+#define VFD_ENTRYSHIFTINCREMENT 0x01
+#define VFD_ENTRYSHIFTDECREMENT 0x00
 
 // flags for display on/off control
-#define LCD_DISPLAYON 0x04
-#define LCD_DISPLAYOFF 0x00
-#define LCD_CURSORON 0x02
-#define LCD_CURSOROFF 0x00
-#define LCD_BLINKON 0x01
-#define LCD_BLINKOFF 0x00
+#define VFD_DISPLAYON 0x04
+#define VFD_DISPLAYOFF 0x00
+#define VFD_CURSORON 0x02
+#define VFD_CURSOROFF 0x00
+#define VFD_BLINKON 0x01
+#define VFD_BLINKOFF 0x00
 
 // flags for display/cursor shift
-#define LCD_DISPLAYMOVE 0x08
-#define LCD_CURSORMOVE 0x00
-#define LCD_MOVERIGHT 0x04
-#define LCD_MOVELEFT 0x00
+#define VFD_DISPLAYMOVE 0x08
+#define VFD_CURSORMOVE 0x00
+#define VFD_MOVERIGHT 0x04
+#define VFD_MOVELEFT 0x00
 
 // flags for function set
-#define LCD_8BITMODE 0x10
-#define LCD_4BITMODE 0x00
-#define LCD_2LINE 0x08
-#define LCD_1LINE 0x00
-#define LCD_5x10DOTS 0x04
-#define LCD_5x8DOTS 0x00
+#define VFD_2LINE 0x08
+#define VFD_1LINE 0x00
+#define VFD_BRIGHTNESS25 0x03
+#define VFD_BRIGHTNESS50 0x02
+#define VFD_BRIGHTNESS75 0x01
+#define VFD_BRIGHTNESS100 0x00
 
-#define LCD_SPICOMMAND 0xF8
-#define LCD_SPIDATA 0xFA
+#define VFD_SPICOMMAND 0xF8
+#define VFD_SPIDATA 0xFA
 
 class SPI_VFD : public Print {
 public:
-  SPI_VFD(uint8_t data, uint8_t clock, uint8_t chipselect);
-  SPI_VFD(uint8_t data, uint8_t clock);
+  SPI_VFD(uint8_t data, uint8_t clock, uint8_t strobe, uint8_t brightness = VFD_BRIGHTNESS100);
     
-  void init(uint8_t data, uint8_t clock, uint8_t chipselect);
-  void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS);
+  void init(uint8_t data, uint8_t clock, uint8_t strobe, uint8_t brightness);
+  void begin(uint8_t cols, uint8_t rows, uint8_t brightness = VFD_BRIGHTNESS100);
 
   void clear();
   void home();
 
+  void setBrightness(uint8_t brightness);
+  uint8_t getBrightness();
   void noDisplay();
   void display();
   void noBlink();
@@ -74,9 +75,9 @@ public:
   virtual void write(uint8_t);
   void command(uint8_t);
 private:
-  void send(uint8_t data);
+  inline void send(uint8_t data);
   
-  uint8_t _clock, _data, _chipselect;  // SPI interface
+  uint8_t _clock, _data, _strobe;  // SPI interface
 
   uint8_t _displayfunction;
   uint8_t _displaycontrol;
@@ -84,7 +85,7 @@ private:
 
   uint8_t _initialized;
 
-  uint8_t _numlines,_currline;
+  uint8_t _numlines, _currline;
 };
 
 #endif
